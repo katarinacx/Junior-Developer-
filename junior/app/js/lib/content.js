c5cbffd47675eec238beeb6473bc755bbbf99231
Tarin Chance <katarinacx@hotmail.com>
2017-07-11 22:37:58 +0100

    Updated

--- junior/app/js/lib/content.js
+++ junior/app/js/lib/content.js
@@ -15,58 +15,56 @@
  JSON parser and event handler
  =====================================================
 */
+//=> refers to the word "function"
+(((window, $) => {
+      const ContentInstance = function( strDataLocation ) {
+       let objContent = {};
+       const arrOnReady = [];
+       let blReady = false;
 
-(function( window, $ ) {
-      var ContentInstance = function( strDataLocation ) {
-        var objContent = {},
-            arrOnReady = [],
-            blReady = false;
+       /**
+        * Get the JSON file
+        */
+       $.getJSON( strDataLocation,
+           objResponse => {
+             objContent = objResponse;
+             blReady = true;
 
-        /**
-         * Get the JSON file
-         */
-        $.getJSON( strDataLocation,
-            function( objResponse ) {
-              objContent = objResponse;
-              blReady = true;
+             /**
+              * Execute all the ready functions once loaded
+              */
+             $.each( arrOnReady,
+                 (intIndex, funDoOnReady) => {
+                   funDoOnReady.call();
+                 }
+             );
+           }
+       );
 
-              /**
-               * Execute all the ready functions once loaded
-               */
-              $.each( arrOnReady,
-                  function( intIndex, funDoOnReady ) {
-                    funDoOnReady.call();
-                  }
-              );
-            }
-        );
+       /**
+        * Register a function to execute once loaded
+        */
+       this.onReady = funDoOnReady => {
+         if( blReady ) {
+           funDoOnReady.call();
+         } else {
+           arrOnReady.push( funDoOnReady );
+         }
+       };
 
-        /**
-         * Register a function to execute once loaded
-         */
-        this.onReady = function( funDoOnReady ) {
-          if( blReady ) {
-            funDoOnReady.call();
-          } else {
-            arrOnReady.push( funDoOnReady );
-          }
-        };
+       /**
+        * Get an item from the content data
+        */
+       this.getItem = intItem => objContent[intItem];
 
-        /**
-         * Get an item from the content data
-         */
-        this.getItem = function( intItem ) {
-          return objContent[intItem];
-        };
-
-        return this;
+       return this;
       };
 
       /**
        * Add the ContentInstance method to the global scope
        */
       window.Content = ContentInstance;
-    })( window, jQuery );
+    }))( window, jQuery );
 
 /*
       ,'``.._   ,'``.
